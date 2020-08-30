8089f8754736b01a756ccf54b275d8465849de80
gacuchac <gabriel.cuchacovich@gmail.com>
2020-08-30 10:01:30 -0400

    Unique retirement titles table created using distinct on

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Employees close to retirement and their titles
 SELECT a.emp_no, a.first_name, a.last_name, b.title, b.from_date, b.to_date
 INTO retirement_titles
 FROM employees AS a
@@ -6,13 +7,9 @@ WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY a.emp_no
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
-
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+SELECT DISTINCT ON (emp_no) emp_no, first_name, last_name, title, from_date, to_date
+INTO unique_titles
+FROM public.retirement_titles
+ORDER BY emp_no, from_date desc;
 
 
